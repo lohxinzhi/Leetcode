1230d10dcb7c1aca5131101afb033bfde92f0301
unknown <lxzraizer@gmail.com>
2024-08-15 18:23:01 +0800

    has path sum complete

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
 using System.Runtime.InteropServices.Marshalling;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Xml.XPath;
 
 namespace SortedArray
 {
@@ -32,13 +34,19 @@ namespace SortedArray
             //     System.Console.WriteLine(i);
             // }
 
-            // TreeNode a = new TreeNode(1);
-            // TreeNode b = new TreeNode(2);
-            // TreeNode c = new TreeNode(3,a,b);
-            // TreeNode e = new TreeNode(4);
-            // TreeNode d = new TreeNode(5);
-            // TreeNode f = new TreeNode(6,e,d);
-            // TreeNode g = new TreeNode(7,c,f);
+
+
+            TreeNode a = new TreeNode(7);
+            TreeNode b = new TreeNode(2);
+            TreeNode c = new TreeNode(13);
+            TreeNode d = new TreeNode(1);
+            TreeNode e = new TreeNode(11,a,b);
+            TreeNode f = new TreeNode(4,null,d);
+            TreeNode g = new TreeNode(4,e);
+            TreeNode h = new TreeNode(8,c,f);
+            TreeNode i = new TreeNode(5,g,h);
+
+            System.Console.WriteLine(HasPathSum(i,18));
             
             // TreeNode d = new TreeNode(4);
             // TreeNode a = new TreeNode(1);
@@ -47,9 +55,9 @@ namespace SortedArray
 
             // // TreeNode h = new TreeNode(8);
 
-
-            // // IList<int> something = PreorderTraversal(g);
-            // IList<int> something = PostorderTraversal(c);
+            // IList<int> something = InorderTraversal(g);
+            // // // IList<int> something = PreorderTraversal(g);
+            // // IList<int> something = PostorderTraversal(c);
             // foreach(int i in something){
             //     System.Console.WriteLine(i);
             // }
@@ -66,7 +74,24 @@ namespace SortedArray
             // System.Console.WriteLine(GrayCode(4));
 
             // System.Console.WriteLine(Candy([29,51,87,87,72,12]));
-            System.Console.WriteLine(Candy([1,6,10,8,7,3,2]));
+         
+            // System.Console.WriteLine(Candy([1,5,4,5,10,10,20,5]));
+
+            // System.Console.WriteLine(Candy([1,6,10,8,7,3,2]));
+            // System.Console.WriteLine(ConvertToTitle(701));
+
+            // var smt = RestoreIpAddresses("00000");
+            // foreach(string i in smt){
+            //     System.Console.WriteLine(i);
+
+            // var xinzhi = new Dictionary<int,int>{};
+            // xinzhi.Add(1,1);
+            // xinzhi.Add(2,2);
+            // xinzhi.Add(3,3);
+            // xinzhi.Remove(1);
+            // foreach(var i in xinzhi){
+            //     System.Console.WriteLine(i.Key);
+            // }
         }
         
         static int Reverse(int x){
@@ -313,6 +338,47 @@ namespace SortedArray
                 }
                 return result;
         }
+        
+        static IList<int> InorderTraversal(TreeNode root){
+            var branchUp = new List<TreeNode>{};
+            var result = new List<int> {};
+
+            if (root==null){
+                return result;
+            }
+
+            while(branchUp.Count>0 || root.right != null || root.left != null){
+                if(root.left != null){
+                    branchUp.Add(root);
+                    root = root.left;
+                }
+                else{ // if left is null
+                    result.Add(root.val);
+                    if (root.right != null){
+                        root=root.right;
+                    }
+                    else{ // root.right == null                       
+                        while(root.right == null && branchUp.Count > 0){
+                            root = branchUp.Last();
+                            branchUp.RemoveAt(branchUp.Count-1);
+                            result.Add(root.val);
+                        }
+                        if (root.right != null){
+                            root = root.right;
+                        }
+                        else{
+                            break;
+                        }
+                    }
+
+                }
+            }
+            if(root.left==null){
+                result.Add(root.val);
+            }
+            return result;
+        }
+
         static int CountDigitOne(int n) {
             // build up from 0 []
             int power; // rounded down
@@ -493,63 +559,242 @@ namespace SortedArray
             return result;
         }
 
-        static int Candy(int[] ratings){
-            int min = 0;
-            int result = 0;
-            int[] values = new int[ratings.Length]; 
+        static int Candy( int[] ratings){
+            if(ratings.Length<=1){
+                return ratings.Length;
+            }
+            int[] values = new int[ratings.Length];
+            Array.Fill<int> (values, 1);
+
+            for (int i = 1; i<ratings.Length; i++){
+                if(ratings[i]>ratings[i-1]){
+                    values[i]=values[i-1]+1;
+                }
+            }
+
+            for(int i = ratings.Length-2; i>=0; i--){
+                if (ratings[i]> ratings[i+1]){
+                    values[i] = Math.Max(values[i], values[i-1]+1);
+                }
+            }
+
+            return values.Sum();
+        }
+
+        static int Candy1(int[] ratings){
             int temp = 0;
+            int prev_temp = 0;
+            int curr_min = 0;
+            int prev_min = 0;
+            int length = ratings.Length;
             int stop = 0;
-            for(int i=1; i<ratings.Length; i++){
-                if (ratings[i] > ratings[i-1]){
-                    temp+=1;
-                }
-                else if (ratings[i] < ratings[i-1]){
-                    temp-=1;
-                    // TODO: split and evaluate
-                }
-                else{ // equal
-                    if (i+1>=ratings.Length){
+            int[] values = new int[length];
+            int result = 0;
+            values[0] = 0;
+            for(int i = 1; i < length; i++){
+                if (ratings[i] > ratings[i-1]){ // if increasing
+                    if( i < length-1 ? ratings[i] > ratings[i+1] : false){ // if at peak
+                        // evaluate result from stop to i-1
+                        result += (1-curr_min) * (i-stop) ;
+                        for (int j = stop; j<i; j++){
+                            values[j] += 1-curr_min;
+                        }
+                        if ( stop>0 ? ratings[stop] > ratings[stop-1] : false){
+                            values[stop] = Math.Max(1-curr_min, prev_temp+1-prev_min);
+                            result+= Math.Max(1-curr_min, prev_temp+1-prev_min) - (1-curr_min); 
+                        }
+                        //update
+                        stop = i;
+                        prev_min = curr_min;
+                        curr_min = 0;
+                        prev_temp = temp+1;
                         temp = 0;
+                        values[i] = temp;
                     }
                     else{
-                            values[i] = min;
-                            temp = 0;
-                            for (int j = stop; j<i; j++){
-                                values[j]+=1-min;
-                            }
-                            result += (1-min) * (i-stop);
-                            if(ratings[stop]<=ratings[stop+1]){
-                                result+=min;
-                            }
-                            if(ratings[i]<ratings[i-1]){
-                                result+= min-temp;
-                            }
-                            min = 0;
-                            stop = i;
-                            continue;
-                            
-                        
+                        temp++;
+                        // curr_min = Math.Min(temp, curr_min); // MAYBE NO NEED
+                        result+=temp;
+                        values[i] = temp;
                     }
                 }
+                else if (ratings[i] < ratings[i-1]){
+                    temp--;
+                    curr_min = Math.Min(temp, curr_min);
+                    result+=temp;
+                    values[i] = temp;
+                }
+                else{ // if equals to prev
+                    result += (1-curr_min) * (i-stop);
+                    for(int j = stop; j<i;j++){
+                        values[j] += 1-curr_min;
+                    }
+                    if ( stop>0 ? ratings[stop] > ratings[stop-1] : false){
+                        values[stop] = Math.Max(1-curr_min, prev_temp+1-prev_min);
+                        result+= Math.Max(1-curr_min, prev_temp+1-prev_min) - (1-curr_min); 
+                    }
 
-                min = Math.Min(min, temp);
-                result+=temp;
-                values[i] = temp;
+                    stop = i;
+                    prev_min = curr_min;
+                    curr_min = 0;
+                    temp = 0;
+                    values[i] = temp;
+                }
             }
-            for (int i=stop;i<ratings.Length;i++){
-                values[i]+=1-min;
+            for(int j = stop; j<length; j++){
+                values[j] += 1-curr_min;
             }
+            result += (1-curr_min) * (length-stop);
+            if ( stop>0 ? ratings[stop] > ratings[stop-1] : false){
+                values[stop] = Math.Max(1-curr_min, prev_temp+1-prev_min);
+                result+= Math.Max(1-curr_min, prev_temp+1-prev_min) - (1-curr_min); 
+            }
+            
 
-            result += (1-min) * (ratings.Length-stop);
+            for(int i = 0; i< length; i++){
+                System.Console.Write($"{ratings[i]}  ");
+            }
+            System.Console.WriteLine();
 
-            if(ratings[stop]<=ratings[stop+1]){
-                result+=min;
+            for(int i = 0; i< length; i++){
+                System.Console.Write($"{values[i]}  ");
             }
-            if(ratings[ratings.Length-1] < ratings[ratings.Length-2]){
-                result+= min-temp;
-            }
+            System.Console.WriteLine();
+
             return result;
         }
+        
+        static string ConvertToTitle(int columnNumber){
+            StringBuilder sb = new StringBuilder();
+            while(columnNumber>0){
+                char temp;
+                if(columnNumber%26 == 0){
+                    temp = 'Z';
+                    columnNumber--;
+                }
+                else{
+                    temp = (char)('A'-1+columnNumber%26);
+                }
+                sb.Insert(0, temp);
+                columnNumber/=26;
+            }
+            return sb.ToString();
+        }
+
+        static IList<string> RestoreIpAddresses(string s) {
+            var addresses = new List<string>{};
+            int length = s.Length;
+
+            int[] ip = new int[4];
+
+            for (int i = 0; i<3; i++){
+                if(length-i-1>9 || length-i-1<3){
+                    continue;
+                }
+                int temp;
+                if (!int.TryParse(s.Substring(0,i+1),out temp)){
+                    break;
+                }
+                if(temp > 255){
+                    break;
+                }
+                ip[0] = temp;
+                for(int j = i+1; j<i+4; j++){
+                    if(length-j-1>6 || length-j-1<2){
+                        continue;
+                    }
+                    if (!int.TryParse(s.Substring(i+1,j-i), out temp)){
+                        break;
+                    }
+                    if(temp > 255){
+                        break;
+                    }
+                    ip[1] = temp;
+                    for(int k = j+1; k<j+4;k++){
+                        if (length-k-1>3 || length-k-1<1){
+                            continue;
+                        }
+                        if (!int.TryParse(s.Substring(j+1, k-j),out temp)){
+                            break;
+                        }
+                        if (temp > 255 || (temp != 0 && s[j+1].Equals('0'))){
+                            break;
+                        }
+                        ip[2] = temp;
+                        if(!int.TryParse(s.Substring(k+1),out temp)){
+                            break;
+                        }
+                        if (temp > 255){
+                            continue;
+                        }
+
+                        if(temp!=0 && s[k+1].Equals('0')){
+                            if(ip[2] == 0){
+                                break;
+                            }
+                            continue;
+                        }
+                        if(temp == 0 && length-k-1>1){
+                            break;
+                        }
+
+                        ip[3] = temp;
+                        addresses.Add(string.Join('.', ip));
+
+                        if(ip[2] == 0){
+                            break;
+                        }
+                    }
+                    if(ip[1] == 0){
+                        break;
+                    }
+                }
+                if(ip[0] == 0){
+                    break;
+                }
+            }
+
+            return addresses;
+        }
+
+        static bool HasPathSum(TreeNode root, int targetSum) {
+            
+            if (root == null){
+                return false;
+            }
+
+
+            int sum = root.val;
+            var branch = new Dictionary<TreeNode,int>{};
+            while(true){
+                if(root.right != null){
+                    branch.Add(root.right,sum+root.right.val);
+                }
+                if (root.left != null){
+                    root = root.left;
+                    sum += root.val;
+                }
+                else if (root.right != null){
+                    root = branch.Last().Key;
+                    sum = branch[root];
+                    branch.Remove(root);
+                }
+                if (root.left == null && root.right == null){
+                    if (sum == targetSum){
+                        return true;
+                    }
+                    else{
+                        if(branch.Count == 0){
+                            return false;
+                        }
+                        root = branch.Last().Key;
+                        sum = branch[root];
+                        branch.Remove(root);
+                    }
+                }
+            }
+        }
+
 
 
     }
